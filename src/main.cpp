ffb417da2ef4d9ab6c3f0e42287cfb178aad0bcf
jinchenglee <jincheng.li@gmail.com>
2017-09-23 21:13:58 -0700

    Experiment to use Frenet coordinate s to control egocar to stay within current lane. There's no control in speed etc. and there's no compliances to speed/acceleration/jerk either, thus there's collision. This is the very first baby step.

--- src/main.cpp
+++ src/main.cpp
@@ -250,41 +250,51 @@ int main() {
 //            }
 
             // Experiment 2: Go in circle
-            double pos_x;
-            double pos_y;
-            double angle;
-            int path_size = previous_path_x.size();
-            std::cout << "previous path size = " << path_size << std::endl;
+//            double pos_x;
+//            double pos_y;
+//            double angle;
+//            int path_size = previous_path_x.size();
+//            std::cout << "previous path size = " << path_size << std::endl;
+//
+//            for(int i = 0; i < path_size; i++)
+//            {
+//                next_x_vals.push_back(previous_path_x[i]);
+//                next_y_vals.push_back(previous_path_y[i]);
+//            }
+//
+//            if(path_size == 0)
+//            {
+//                pos_x = car_x;
+//                pos_y = car_y;
+//                angle = deg2rad(car_yaw);
+//            }
+//            else
+//            {
+//                pos_x = previous_path_x[path_size-1];
+//                pos_y = previous_path_y[path_size-1];
+//
+//                double pos_x2 = previous_path_x[path_size-2];
+//                double pos_y2 = previous_path_y[path_size-2];
+//                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+//            }
+//
+//            double dist_inc = 0.5;
+//            for(int i = 0; i < 50-path_size; i++)
+//            {
+//                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
+//                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
+//                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+//                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+//            }
 
-            for(int i = 0; i < path_size; i++)
+            // Experiment 3: Stay in current lane as if there's only ego car
+            vector<double> XY(2);
+            double dist_inc = 0.4;
+            for(int i = 0; i < 50; i++)
             {
-                next_x_vals.push_back(previous_path_x[i]);
-                next_y_vals.push_back(previous_path_y[i]);
-            }
-
-            if(path_size == 0)
-            {
-                pos_x = car_x;
-                pos_y = car_y;
-                angle = deg2rad(car_yaw);
-            }
-            else
-            {
-                pos_x = previous_path_x[path_size-1];
-                pos_y = previous_path_y[path_size-1];
-
-                double pos_x2 = previous_path_x[path_size-2];
-                double pos_y2 = previous_path_y[path_size-2];
-                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-            }
-
-            double dist_inc = 0.5;
-            for(int i = 0; i < 50-path_size; i++)
-            {
-                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
-                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
-                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
-                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+                  XY = getXY(car_s+(dist_inc*i), car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                  next_x_vals.push_back(XY[0]);
+                  next_y_vals.push_back(XY[1]);
             }
 
             // END TODO
@@ -294,7 +304,7 @@ int main() {
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
-          	//this_thread::sleep_for(chrono::milliseconds(1000));
+            //this_thread::sleep_for(chrono::milliseconds(500));
           	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
           
         }
